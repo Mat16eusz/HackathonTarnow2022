54104a8e78875d3128dbf41b8f97c08d1fbaf6ee
Janusz Zychal <56130048+bigjohn98pl@users.noreply.github.com>
2022-11-27 11:42:32 +0100

    viewHomes: - add view, - add icons to view bar, - add home, - add recycle wiew layout for home elements

---
+++ app/src/main/java/com/example/hackathon2022/ui/HomeListActivity.kt
@@ -0,0 +1,17 @@
+package com.example.hackathon2022.ui
+
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.hackathon2022.R
+import com.example.hackathon2022.common.base.BaseActivity
+import com.example.hackathon2022.databinding.ActivityHomeListBinding
+
+class HomeListActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityHomeListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home_list)
+    }
+}

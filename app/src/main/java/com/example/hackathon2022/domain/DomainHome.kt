f310d647205183009463305120d7b22d0aaf3bad
Janusz Zychal <56130048+bigjohn98pl@users.noreply.github.com>
2022-11-28 15:17:31 +0100

    Dev: - update

--- app/src/main/java/com/example/hackathon2022/domain/DomainHome.kt
+++ app/src/main/java/com/example/hackathon2022/domain/DomainHome.kt
@@ -3,6 +3,6 @@ package com.example.hackathon2022.domain
 data class DomainHome(
     var id: Int? = null,
     var homeName: String = "",
-    var usagePower: Double? = null,
+    var usagePower: Double? = 0.00,
     var devices: MutableList<DomainDevice> = mutableListOf(),
 )
